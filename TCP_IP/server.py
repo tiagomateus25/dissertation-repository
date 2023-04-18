d542515fc4a2d05173560e0a4e9bb0d5fb60ec6c
tiago <tiagomateus.fonte13@ua.pt>
2023-04-18 18:20:53 +0100

    updated

--- TCP_IP/server.py
+++ TCP_IP/server.py
@@ -11,16 +11,22 @@ sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 server_address = ('192.168.56.237', 10000)
 print('starting up on {} port {}'.format(*server_address))
 sock.bind(server_address)
-
+print('Waiting for communication.')
 # Listen for incoming connections
 sock.listen(1)
+# connection, client_address = sock.accept()
 
 while True:
 
     connection, client_address = sock.accept()
 
     # Receive the data in small chunks and retransmit it
-    data = connection.recv(5000)
-    data = json.loads(data.decode())
-    print('Value received: ', data)
-    time.sleep(0.01)
+    data = connection.recv(1024)
+    # data = json.loads(data.decode())
+    # print(type(data))
+
+    print('Value received: ', data.decode('utf8'))
+    # print('Value received: ', json.loads(data.decode()))
+
+    # time.sleep(1)
+    # connection.close()
