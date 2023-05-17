20214cb65d168068df973676c81cf9d203166b86
tiago <tiagomateus.fonte13@ua.pt>
2023-05-17 12:57:16 +0100

    added plot render to simple env

--- simple_1DOF_env/simple_1DOF_env.py
+++ simple_1DOF_env/simple_1DOF_env.py
@@ -1,14 +1,17 @@
 #!/usr/bin/env python3
+import gymnasium as gym
 from gym import Env
 from gym.spaces import Discrete, Box
 import numpy as np
 import matlab.engine
+from typing import Optional
 import time
+import matplotlib.pyplot as plt
 
 eng = matlab.engine.start_matlab()
 
 class simple_1DOF_env(Env):
-    def __init__(self):
+    def __init__(self, render_mode: Optional[str] = None):
 
         # frequencies
         self.init_freq = 1
@@ -42,6 +45,11 @@ class simple_1DOF_env(Env):
         # observation space
         self.observation_space = Box(low, high, dtype=np.float32)
 
+        # render
+        self.render_mode = render_mode
+        self.x = []
+        self.y = []
+
     def reset(self):
         # reset state
         self.state = np.array([self.init_freq, 0], dtype=np.float32)
@@ -56,6 +64,10 @@ class simple_1DOF_env(Env):
         # calculate average power
         AveragePower = eng.avPow(self.current_state[0], action+1)
         self.state = np.array([self.init_freq, AveragePower], dtype=np.float32)
+
+        # rendering
+        if self.render_mode == 'human':
+            self.render()
 
         # check if truncated
         self.steps += 1
@@ -76,33 +88,57 @@ class simple_1DOF_env(Env):
         # return
         return np.array(self.state, dtype=np.float32), reward, terminated, truncated, info
        
-    def render():
-        pass
+    def render(self):
+        if self.render_mode is None:
+            gym.logger.warn(
+                "You are calling render method without specifying any render mode. "
+                "You can specify the render_mode at initialization, "
+                f'e.g. gym("{self.spec.id}", render_mode="rgb_array")'
+            )
+            return
+        
+        if self.render_mode == 'human':
+            
+            # x axis: step
+            self.x.append(self.steps)
 
+            # y axis: average power
+            self.y.append(self.state[1])
+            
     def close(self):
-        pass
+        # check values
+        # print(self.x)
+        # print(self.y)
 
+        # plot results
+        plt.plot(self.x, self.y, 'b-')
+        self.x = []
+        self.y = []
+    
 
 # test env
 
-# env = simple_1DOF_env()
+env = simple_1DOF_env(render_mode='human')
 
-# episodes = 1
-# for episode in range(1, episodes+1):
-#     state = env.reset()
-#     terminated = False
-#     truncated = False
-#     score = 0
-#     steps = 0
-#     start = time.time()
-#     while not terminated and not truncated:
-#         action = env.action_space.sample()
-#         n_state, reward, terminated, truncated, info = env.step(action)
-#         score += reward
-#         steps+= 1
-#     print('Episode:{} Score:{}'.format(episode, score))
-#     print('Number of steps:', steps)
-#     end = time.time()
-#     print('Elapsed time is', end - start, 'seconds.')
-# env.close()
+episodes = 5
+for episode in range(1, episodes+1):
+    state = env.reset()
+    terminated = False
+    truncated = False
+    score = 0
+    steps = 0
+    start = time.time()
+    while not terminated and not truncated:
+        action = env.action_space.sample()
+        n_state, reward, terminated, truncated, info = env.step(action)
+        score += reward
+        steps+= 1
+    print('Episode:{} Score:{}'.format(episode, score))
+    print('Number of steps:', steps)
+    end = time.time()
+    print('Elapsed time is', end - start, 'seconds.')
+    env.close()
+    if episode != episodes:
+        plt.close()
+    plt.show()
 
